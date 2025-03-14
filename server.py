3994bd903bbccf51183b8b7e342a2fab6c5bae78
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-03-14 18:05:55 +0000

    updated
    Guess age and gender completed

--- server.py
+++ server.py
@@ -1,10 +1,29 @@
-from flask import Flask
+import random
+import requests
+from datetime import datetime
+from flask import Flask, render_template
+
+AGIFY_URL = "https://api.agify.io?name="
+GENDERIZE_URL = "https://api.genderize.io?name="
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def home():
-    return "Hello World!"
+    random_number = random.randint(1, 10)
+    year = datetime.now().year
+    return render_template("index.html", num=random_number, year=year)
+
+@app.route("/guess/<name>")
+def age_gender(name):
+    agify_response = requests.get(url = f"{AGIFY_URL}{name}")
+    genderize_response = requests.get(url = f"{GENDERIZE_URL}{name}")
+    agify_data = agify_response.json()
+    genderize_data = genderize_response.json()
+    return render_template("guess.html",
+                           name=name, age=agify_data["age"],
+                           gender=genderize_data["gender"])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
